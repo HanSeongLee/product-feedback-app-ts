d9ac06b6d7288db0cc875d8af342535fe075d49b
HanSeongLee <jkhanseong@gmail.com>
2022-10-26 23:05:09 +0900

    ADD | Error box to Input component

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,12 +1,24 @@
 import React, { InputHTMLAttributes } from 'react';
 import styles from './style.module.scss';
+import cn from 'classnames';
 
-const Input = React.forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(({ ...props }, ref) => {
+interface IProps extends InputHTMLAttributes<HTMLInputElement> {
+    error?: string;
+}
+
+const Input = React.forwardRef<HTMLInputElement, IProps>(({ error, ...props }, ref) => {
     return (
-        <input className={styles.input}
-               {...props}
-               ref={ref}
-        />
+        <div className={cn(styles.inputWrapper, {
+            [styles.error]: error,
+        })}>
+            <input className={styles.input}
+                   {...props}
+                   ref={ref}
+            />
+            <div className={styles.errorBox}>
+                {error}
+            </div>
+        </div>
     );
 });
 
