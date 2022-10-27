07e641e033253cbf45c3f281e54c3e9752109936
HanSeongLee <jkhanseong@gmail.com>
2022-10-27 21:33:07 +0900

    ADD | Error box to TextArea component

--- components/TextArea/index.tsx
+++ components/TextArea/index.tsx
@@ -1,14 +1,26 @@
 import React, { TextareaHTMLAttributes } from 'react';
 import styles from './style.module.scss';
+import cn from 'classnames';
 
-const TextArea = React.forwardRef<HTMLTextAreaElement, TextareaHTMLAttributes<HTMLTextAreaElement>>(({ children, ...props }, ref) => {
+interface IProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+    error?: string;
+}
+
+const TextArea = React.forwardRef<HTMLTextAreaElement, IProps>(({ error, children, ...props }, ref) => {
     return (
-        <textarea className={styles.textarea}
-                  {...props}
-                  ref={ref}
-        >
-            {children}
-        </textarea>
+        <div className={cn(styles.textareaWrapper, {
+            [styles.error]: error,
+        })}>
+            <textarea className={styles.textarea}
+                      {...props}
+                      ref={ref}
+            >
+                {children}
+            </textarea>
+            <div className={styles.errorBox}>
+                {error}
+            </div>
+        </div>
     );
 });
 
