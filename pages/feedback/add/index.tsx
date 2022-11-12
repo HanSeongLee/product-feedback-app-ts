6e51e5dd1e796de83dda683d995eb3f4f53d7963
HanSeongLee <jkhanseong@gmail.com>
2022-11-12 23:29:02 +0900

    ADD | Authentication redirect to adding feedback page

--- pages/feedback/add/index.tsx
+++ pages/feedback/add/index.tsx
@@ -1,8 +1,10 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import styles from './style.module.scss';
 import Container from 'components/Container';
 import Navigator from 'components/Navigator';
 import NewFeedbackFormContainer from 'containers/NewFeedbackFormContainer';
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from 'pages/api/auth/[...nextauth]';
 
 const FeedbackAddPage: NextPage = () => {
     return (
@@ -17,5 +19,26 @@ const FeedbackAddPage: NextPage = () => {
         </>
     );
 };
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const session = await unstable_getServerSession(
+        context.req,
+        context.res,
+        authOptions
+    );
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: `/auth/signin?callbackUrl=${context.resolvedUrl}`,
+                permanent: false,
+            },
+        }
+    }
+
+    return {
+        props: {},
+    };
+}
 
 export default FeedbackAddPage;
