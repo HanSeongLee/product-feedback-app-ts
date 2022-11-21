f85589ae8d0bef138be930b743876e62e9dfc010
HanSeongLee <jkhanseong@gmail.com>
2022-11-21 22:58:11 +0900

    ADD | Theme and title to Navigator component

--- components/Navigator/index.tsx
+++ components/Navigator/index.tsx
@@ -6,25 +6,38 @@ import Link from 'next/link';
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
     goBackUrl: string;
+    title?: string;
+    theme?: 'light' | 'dark';
 }
 
-const Navigator: React.FC<IProps> = ({ goBackUrl, className, children, ...props }) => {
+const Navigator: React.FC<IProps> = ({ goBackUrl, title, theme, className, children, ...props }) => {
     return (
-        <div className={cn(styles.navigator, className)}
+        <div className={cn(styles.navigator, {
+            [styles.dark]: theme === 'dark',
+        }, className)}
              {...props}
         >
             <div className={styles.leftSide}>
                 <Link href={goBackUrl}>
                     <a className={styles.goBackButton}>
-                        <ArrowLeftIcon /> Go Back
+                        <ArrowLeftIcon className={styles.icon} /> Go Back
                     </a>
                 </Link>
+                {title && (
+                    <div className={styles.title}>
+                        {title}
+                    </div>
+                )}
             </div>
             <div>
                 {children}
             </div>
         </div>
     );
+};
+
+Navigator.defaultProps = {
+    theme: 'light',
 };
 
 export default Navigator;
