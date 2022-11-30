7d73adccc892291a493be95b01ddf33bfded0749
HanSeongLee <jkhanseong@gmail.com>
2022-12-01 06:36:34 +0900

    ADD | SuggestionBar component on tablet

--- components/SuggestionBar/index.tsx
+++ components/SuggestionBar/index.tsx
@@ -5,10 +5,23 @@ import Container from 'components/Container';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { SORT_BY_DEFAULT_VALUE, sortParamMap } from 'lib/constants';
+import SuggestionsIcon from '/public/icons/icon-suggestions.svg';
+import { useStore } from 'lib/store';
+import shallow from 'zustand/shallow';
+
+const useFeedback = () => {
+    return useStore(
+        (store) => ({
+            feedbackList: store.feedbackList,
+        }),
+        shallow
+    );
+};
 
 const SuggestionBar: React.FC = () => {
     const router = useRouter();
     const [sortBy, setSortBy] = useState(SORT_BY_DEFAULT_VALUE);
+    const { feedbackList } = useFeedback();
 
     const handleSortBy = useCallback((newValue: string) => {
         if (newValue in sortParamMap) {
@@ -50,30 +63,36 @@ const SuggestionBar: React.FC = () => {
     return (
         <div className={styles.suggestionBar}>
             <Container className={styles.container}>
-                <Select className={styles.select}
-                        valueClassName={styles.value}
-                        label={'Sort by'}
-                        value={sortBy}
-                        onChange={handleSortBy}
-                        options={[
-                            {
-                                label: 'Most Upvotes',
-                                value: '0',
-                            },
-                            {
-                                label: 'Least Upvotes',
-                                value: '1',
-                            },
-                            {
-                                label: 'Most Comments',
-                                value: '2',
-                            },
-                            {
-                                label: 'Least Comments',
-                                value: '3',
-                            },
-                        ]}
-                />
+                <div className={styles.leftSide}>
+                    <div className={styles.title}>
+                        <SuggestionsIcon className={styles.icon} />
+                        {feedbackList?.length} Suggestions
+                    </div>
+                    <Select className={styles.select}
+                            valueClassName={styles.value}
+                            label={'Sort by'}
+                            value={sortBy}
+                            onChange={handleSortBy}
+                            options={[
+                                {
+                                    label: 'Most Upvotes',
+                                    value: '0',
+                                },
+                                {
+                                    label: 'Least Upvotes',
+                                    value: '1',
+                                },
+                                {
+                                    label: 'Most Comments',
+                                    value: '2',
+                                },
+                                {
+                                    label: 'Least Comments',
+                                    value: '3',
+                                },
+                            ]}
+                    />
+                </div>
 
                 <Link href={'/feedback/add'}>
                     <a className={styles.addFeedbackButton}>
