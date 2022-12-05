44eac43eec999d6b09e76223c5ac4eecf65114a8
HanSeongLee <jkhanseong@gmail.com>
2022-12-05 19:12:28 +0900

    ADD | RoadmapColumn component on tablet

--- components/RoadmapColumn/index.tsx
+++ components/RoadmapColumn/index.tsx
@@ -1,14 +1,20 @@
 import React, { HTMLAttributes } from 'react';
 import styles from './style.module.scss';
+import cn from 'classnames';
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
     title: string;
     description: string;
 }
 
-const RoadmapColumn: React.FC<IProps> = ({ title, description, children }) => {
+const RoadmapColumn: React.FC<IProps> = ({
+                                             title, description, children, className,
+                                             ...props
+                                         }) => {
     return (
-        <div className={styles.roadmapColumn}>
+        <div className={cn(styles.roadmapColumn, className)}
+             {...props}
+        >
             <div className={styles.title}>
                 {title}
             </div>
